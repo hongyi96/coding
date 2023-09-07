99fb714fc419d2b1bf589a4d52302a1cbbcb21d5
hongyi <940810647@qq.com>
2023-09-07 20:25:08 +0800

    init

--- leetcode/src/main/java/AddTwoNumbers.java
+++ leetcode/src/main/java/AddTwoNumbers.java
@@ -23,25 +23,8 @@ public class AddTwoNumbers {
         }
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-
-    }
 
 
     public static void main(String[] args) {
-//赋值试卷附属信息
-        for (BusExamPaperDto u : dtoList) {
-            Optional.ofNullable(sessionMap.get(u.getPaperId)).ifPresent(sessionList ->
-                            sessionList.stream()
-                                    .filter(t -> t.getExamStatus() == ExamStatusEnum.RUNNING.getCode())
-                                    .findFirst()
-                                    .ifPresentorELse(t -> {
-                                                u.setExamStatus((byte) ExamStatusEnum.RUNNING.getCode());
-                                                u.setBusExanSessionDTO(t);
-                                            },
-                                            () -> u.setExamstatus((byte) ExamStatusEnum.END.getCode())),
-                    () -> u.setExamStatus((byte) ExamStatusEnum.NO_START.getCode()));
-            return new PageImpl<>(dtoList, pageList.getPageable(), pageList.getTotalELements());
-        }
     }
 }
